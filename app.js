ac9bbb816c25aa5fe913b12451a4d6a7fd02f1f6
Yeseul Lee <dev.yezlee@gmail.com>
2021-06-30 23:49:17 +0900

    Paingting Board made with Vanilla JS

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
-const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
+// Gotta define a pixel modifier!!!
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
-ctx.lineWidth = 2.5;
+ctx.lineWith = 2.5;
+// ctx.fillRect(50, 20, 100, 49);
 
 let painting = false;
 let filling = false;
@@ -40,13 +39,28 @@ function onMouseMove(event) {
   }
 }
 
+// Dont need this anymore
+// function onMouseDown(event) {
+//   painting = true;
+// }
+
+// 매번 이렇게 painting을 false로 만들기 귀찮아서 함수로 만들어서 실행시킴
+// 그래서 이렇게 작성할 필요도 없이 아래 mouseleave할때 stopPainting이라는 함수를 실행시킴 된다.
+// function onMouseLeave(event) {
+//   painting = false;
+// }
+
 function handleColorClick(event) {
+  // console.log(event.target.style);
+  // console.log(event);
   const color = event.target.style.backgroundColor;
-  ctx.strokeStyle = color;
+  ctx.strokeStyle = color; // overiding strokeStyle
   ctx.fillStyle = color;
+  // console.log(color);
 }
 
 function handleRangeChange(event) {
+  // console.log(event.target.value);
   const size = event.target.value;
   ctx.lineWidth = size;
 }
@@ -63,20 +77,8 @@ function handleModeClick() {
 
 function handleCanvasClick() {
   if (filling) {
-    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
-}
-
-function handleCM(event) {
-  event.preventDefault();
-}
-
-function handleSaveClick() {
-  const image = canvas.toDataURL();
-  const link = document.createElement("a");
-  link.href = image;
-  link.download = "PaintJS[🎨]";
-  link.click();
 }
 
 if (canvas) {
@@ -85,12 +87,16 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
-  canvas.addEventListener("contextmenu", handleCM);
 }
 
-Array.from(colors).forEach(color =>
+// console.log(colors); //const colors = document.getElementsByClassName("jsColor");
+// console.log(Array.from(colors));
+
+Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+// forEach로 color를 돌려서 addEventListener("click", handleColorClick)를 호출함
+// color는 매개변수. 그냥 이 array안에 있는 각각의 아이템(여기서는 each div)들을 대표하는 것 뿐임. - color대신에 아무이름 써도 상관없음
 
 if (range) {
   range.addEventListener("input", handleRangeChange);
@@ -98,8 +104,4 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
-}
-
-if (saveBtn) {
-  saveBtn.addEventListener("click", handleSaveClick);
 }
